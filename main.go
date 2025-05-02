63e5aaaf5dc78c2134e5895a6ab020a6364b6092
azaroche <andrey.zar@gmail.com>
2025-05-02 18:58:43 -0500

    delete MetricsExcludeFilterRegex

--- main.go
+++ main.go
@@ -32,7 +32,6 @@ var (
 	slurmDiagEnabled     = flag.Bool("slurm.collect-diags", false, "Collect daemon diagnostics stats from slurm")
 	slurmSacctEnabled    = flag.Bool("slurm.collect-limits", false, "Collect account and user limits from slurm")
 	slurmCliFallback     = flag.Bool("slurm.cli-fallback", true, "drop the --json arg and revert back to standard squeue for performance reasons")
-#	metricsFilterRegex   = flag.String("metrics.exclude", "", "Regex pattern for metrics to exclude")
 )
 
 func main() {
@@ -54,7 +53,6 @@ func main() {
 		SlurmCliFallback:          *slurmCliFallback,
 		TraceRate:                 *traceRate,
 		SlurmAcctOverride:         *slurmSaactOverride,
-#		MetricsExcludeFilterRegex: *metricsFilterRegex,
 	}
 	config, err := exporter.NewConfig(&cliFlags)
 	if err != nil {
